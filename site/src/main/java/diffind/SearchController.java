4b9f0c2b802ed80549b473823b5fd6320ed1ab48
ldy <ldy7175@gmail.com>
2016-02-27 17:43:03 +0800

    [Feature] import elasticsearch jar api to search
    search by condition use transport way of api

--- site/src/main/java/diffind/SearchController.java
+++ site/src/main/java/diffind/SearchController.java
@@ -4,6 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
+import org.elasticsearch.index.query.QueryBuilders;
+import java.net.InetAddress;
+
 
 @Controller
 public class SearchController {
@@ -15,8 +24,25 @@ public class SearchController {
     }
 
     @RequestMapping("/s")
-    public String search(@RequestParam(value="q", required=true) String query, Model model) {
+    public String search(@RequestParam(value="q", required=true) String query, Model model) throws Exception{
+        Client client = TransportClient.builder().build()
+                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("182.254.242.25"), 9300));
+        SearchResponse response = client.prepareSearch("index")
+                .setTypes("post")
+                .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+                .setQuery(QueryBuilders.termQuery("content", "lyc7175"))
+                .setFrom(0).setSize(6).setExplain(false)
+                .setHighlighterPostTags("</em>")
+                .setHighlighterPreTags("<em>")
+                .addHighlightedField("content")
+                .setFetchSource(new String[]{"url"}, null)
+                .execute()
+                .actionGet();
+        System.out.println(response);
+        client.close();
+
         model.addAttribute("query", query);
+        model.addAttribute("response", response);
         return "search";
     }
 
